1256d3b471ba3c162ff75d50208638452f3a7fff
felipeLx <felipealvesfinanceiro@gmail.com>
2021-11-07 15:16:50 +0200

    course: data analysis exercise 2

---
+++ exercises/pandas/dataLoadData.py
@@ -0,0 +1,5 @@
+import pandas as pd
+
+# Read data from CSV file
+csv_path = 'https://cf-courses-data.s3.us.cloud-object-storage.appdomain.cloud/IBMDeveloperSkillsNetwork-PY0101EN-SkillsNetwork/labs/Module%204/data/TopSellingAlbums.csv'
+df = pd.read_csv(csv_path)
