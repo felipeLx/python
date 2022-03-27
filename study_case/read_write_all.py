6f332ffcb64dbfe6a3cbe49ea3dda99121a189ab
felipeLx <felipealvesfinanceiro@gmail.com>
2022-03-27 06:55:54 -0300

    study case: finalized

--- study_case/read_write_all.py
+++ study_case/read_write_all.py
@@ -7,5 +7,5 @@ df = pd.DataFrame()
 for file in os.listdir('csv_ready'):
   if file.endswith('.csv'):
     df = pd.concat([df, pd.read_csv(os.path.join('csv_ready', file), sep=';', encoding='latin1', header=0)])
-    df.to_csv('br_rain_2010-2021.csv', sep=';', index=False)
+    df.to_csv('br_rain_2010-2021.csv', sep=';', index=False                                                                                                                                                                                                                         )
 print(df.info())
