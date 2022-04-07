72e1f073302c886e3b8cff52d3d4668a5021b929
felipeLx <felipealvesfinanceiro@gmail.com>
2022-04-06 21:37:30 -0300

    simple project to use sklearn and streamlit

--- fakenews/main.py
+++ fakenews/main.py
@@ -1,11 +1,12 @@
-from xml.dom import UserDataHandler
 import pandas as pd
 import numpy as np
 from sklearn.feature_extraction.text import CountVectorizer
 from sklearn.model_selection import train_test_split
+from sklearn.feature_extraction.text import TfidfVectorizer
+from sklearn.linear_model import PassiveAggressiveClassifier
 from sklearn.naive_bayes import MultinomialNB
 
-df = pd.read_csv('/home/felipelx/Downloads/fake_or_real_news.csv')
+df = pd.read_csv('./fake_or_real_news.csv')
 
 x = np.array(df["title"])
 y = np.array(df["label"])
@@ -17,7 +18,8 @@ model = MultinomialNB()
 model.fit(x_train, y_train)
 
 import streamlit as st
-st.title("Fake News Detector")
+st.title("Detector de notícias falsas")
+st.markdown("<h5 style='text-align: center; color: #ff0000;'>Copiar lá e colar aqui, a resposta sairá em seguida</h5>", unsafe_allow_html=True)
 
 def fakenews_detector():
     user = st.text_area("Coloque o texto aqui: ")
@@ -29,4 +31,5 @@ def fakenews_detector():
         a = model.predict(data)
         st.title(a)
 
-fakenews_detector()
+if __name__ == "__main__":
+    fakenews_detector()
